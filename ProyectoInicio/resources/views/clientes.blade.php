a746d818cacb09d7d67b2cb163c1117b606c7896
Diegooraul <133446592+Diegooraul@users.noreply.github.com>
2024-09-28 08:55:18 -0600

    TERMINAMOS LA ACTIVIDAD 3
    SE AGREGO UNA ´PLANTILLA, SE ELIMINO LA NAVBAR DE FORMULARIO, Y DE CLIENTES PARA QUE MANDANDOLA LLAMAR CON UN REQUEST Y UN YIELD HACER USO DE LA PLANTILLA

--- ProyectoInicio/resources/views/clientes.blade.php
+++ ProyectoInicio/resources/views/clientes.blade.php
@@ -1,33 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    @vite(['resources/js/app.js'])
-    <link rel="stylesheet" href="{{ asset('css/fondo.css') }}">
+@extends('layouts.plantilla1')
 
-    <title>Consultar Clientes</title>
-</head>
-<body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/">Turista sin Maps</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="{{ route('rutacacas') }}">Registro Clientes</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/consultar">Consulta Clientes</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+@section('titulo','Clientes')
+
+@section('contenido')
 
 <div class="container mt-5 col-md-8">
 
@@ -50,5 +25,5 @@
 </div>
 </div>
 
-</body>
-</html>
+
+@endsection
